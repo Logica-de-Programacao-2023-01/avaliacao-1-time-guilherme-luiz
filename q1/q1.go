8c57bff8504c05c95c3076973ef95e8c66eb00ce
guilemouraoc <86135577+guilemouraoc@users.noreply.github.com>
2023-04-25 08:14:12 -0300

    Update q1.go

--- q1/q1.go
+++ q1/q1.go
@@ -12,8 +12,24 @@ package q1
 //
 //A função deve retornar um valor booleano, indicando se é possível ou não dividir a melancia da forma desejada. Se o peso
 //da melancia for menor ou igual a 0, a função deve retornar um erro.
+import "fmt"
 
 func DivideWatermelon(weight int) (bool, error) {
-	// Seu código aqui
-	return false, nil
+
+	if weight <= 0 {
+
+		return false, fmt.Errorf("peso inválido")
+
+	}
+
+	if weight >= 4 && weight%2 == 0 {
+
+		return true, nil
+
+	} else {
+
+		return false, nil
+
+	}
+
 }
